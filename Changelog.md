0601e2aa19b9fbcc3d4fe3799cb51841e9bf08a4
chris <typo3@mediadreams.org>
2025-04-16 19:53:01 +0200

    [TASK] update description of extension and add a changelog

---
+++ Changelog.md
@@ -0,0 +1,8 @@
+# Version 1.0.1 (2025-04-16)
+- [TASK] fix GitHub action for TER release
+
+All changes
+https://github.com/cdaecke/md_notifications/compare/v1.0.0...v1.0.1
+
+# Version 1.0.0 (2025-04-16)
+- Initial release, have fun!
--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,7 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Notifications',
-    'description' => 'Notify frontend users about new data records.',
+    'description' => 'Notify frontend users about the creation of new data records. Every data type can be configured easily, for example pages (blog posts) or tx_news records.',
     'category' => 'fe',
     'author' => 'Christoph Daecke',
     'author_email' => 'typo3@mediadreams.org',
